3f870c0b70fea5ae5d9c9dd7904296da54b72fcc
Terry Chen <radiant107@gmail.com>
2021-07-25 16:59:25 +0800

    headup version

--- m1.py
+++ m1.py
@@ -4,12 +4,15 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入價格： ')
+	price = int(price)
 	merchant.append([name, price])
 print(merchant)
 
 print(merchant[0][0], '的價格是', merchant[0][1], '元') 
 
-with open('merchant.csv', 'w') as f: # 寫入
+with open('merchant.csv', 'w', encoding = 'utf-8') as f: # 寫入
+	f.write('商品,價格\n')
 	for m in merchant:
-		f.write(m[0] + ',' + m[1] + '元' + '\n') # CSV檔在excel中用逗點做區隔，電腦才看得懂會自動分格
+		f.write(m[0] + ',' + str(m[1]) + '\n') # CSV檔在excel中用逗點做區隔，電腦才看得懂會自動分格
+		                                              # 字串跟字串合併中間用+號
 
